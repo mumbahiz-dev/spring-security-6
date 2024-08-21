1ddc30bea79a69f46eab6f42f211a10f5fa94db4
mumbahiz <liqoo@nostratech.com>
2024-08-21 09:14:31 +0700

    form login for mvc / monolitic application

---
+++ form-login-config-mvc-monolitic/src/main/java/com/mumbahiz/mbzschool/MbzSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.mumbahiz.mbzschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbzSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbzSchoolApplication.class, args);
+	}
+
+}
---
+++ form-login-config-mvc-monolitic/src/test/java/com/mumbahiz/mbzschool/MbzSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumbahiz.mbzschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbzSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
