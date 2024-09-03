d12bd20c0f09058fd60b522fdbddc86d203529e9
mumbahiz <liqoo@nostratech.com>
2024-09-03 10:47:29 +0700

    add mbz bank BE

---
+++ config-cors-csrf/mbzbank-be/src/main/java/com/mumbahiz/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.mumbahiz.repository;
+
+import com.mumbahiz.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+}
