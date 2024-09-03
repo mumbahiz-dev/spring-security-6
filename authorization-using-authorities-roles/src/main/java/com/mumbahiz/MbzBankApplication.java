3c1e673e4daad3e1db941466c44af4dcf26e9820
mumbahiz <liqoo@nostratech.com>
2024-09-03 14:55:33 +0700

    init authorization

---
+++ authorization-using-authorities-roles/src/main/java/com/mumbahiz/MbzBankApplication.java
@@ -0,0 +1,13 @@
+package com.mumbahiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbzBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbzBankApplication.class, args);
+    }
+
+}
