482d5e213b5f35601021f138827f5e158ff3f30f
mumbahiz <liqoo@nostratech.com>
2024-09-09 21:25:50 +0700

    init authentication using jwt

---
+++ authentication-using-jwt/src/test/java/com/mumbahiz/MbzBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumbahiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbzBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
