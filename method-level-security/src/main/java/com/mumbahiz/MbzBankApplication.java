ed59f1026c9857651f2465785862526960f1b3f6
mumbahiz <liqoo@nostratech.com>
2024-09-10 15:34:08 +0700

    method level security PreAuthorize and PostAuthorize

--- method-level-security/src/main/java/com/mumbahiz/MbzBankApplication.java
+++ method-level-security/src/main/java/com/mumbahiz/MbzBankApplication.java
@@ -2,8 +2,10 @@ package com.mumbahiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 public class MbzBankApplication {
 
     public static void main(String[] args) {
--- method-level-security/src/main/java/com/mumbahiz/repository/LoanRepository.java
+++ method-level-security/src/main/java/com/mumbahiz/repository/LoanRepository.java
@@ -3,6 +3,7 @@ package com.mumbahiz.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import com.mumbahiz.model.Loans;
@@ -10,6 +11,7 @@ import com.mumbahiz.model.Loans;
 @Repository
 public interface LoanRepository extends CrudRepository<Loans, Long> {
 	
+	@PreAuthorize("hasRole('USER')")
 	List<Loans> findByCustomerIdOrderByStartDtDesc(long customerId);
 
 }
